d4457dc1fee8e052b7bee048962c11a3b9d72fad
jaredASC4 <jared.antoinee@gmail.com>
2017-07-13 09:33:17 -0400

    Created SI Folders

---
+++ week0/pokemonfunctions.js
@@ -0,0 +1,6 @@
+function randLetter(){
+    var letters = ("a","b","c","d","e","f","g","h","i","j","k" ,"l" ,"m" ,"n" ,"o" ,"p" ,"q" ,"r" ,"s" ,"t" ,"u" ,"v","w","x","y","z");
+    var letter = letters(Math.floor(Math.random() * letters.length));
+    return letter
+    console.log(randLetter);
+}
